4a4a84421908752dac6614ccc1d3a5b3a02387f3
plugmcpe <M7mmdgamer7@icloud.com>
2017-06-28 07:42:29 +0300

    Create Main.php

---
+++ src/easyCommand/Main.php
@@ -0,0 +1,23 @@
+<?php
+use pocketmine\plugin\PluginBase;
+use pocketmine\command\CommandSender;
+use pocketmine\command\Command;
+class Main extends PluginBase{
+public function onEnable(){
+ $this->getServer()->getLogger()->info("plugin easyCommand has been enabled");
+ $this->getLogger()->info("plugin easyCommand by @plugmcpe");
+ }
+ public function onDisable(){
+  $this->getServer()->getLogger()->info("plugin easyCommand has been disable");
+  }
+ public function onCommand(CommandSender $sender, Command $cmd, $label, array $args){
+   switch ($cmd->getName()){
+    case 'g1':
+    $player = $sender->getPlayer();
+      $player->setGamemode(1);
+      break;
+     case 'g0':
+     $player = $sender->getPlayer();
+       $player->setGamemode(0);
+       break;
+      case 'f':
